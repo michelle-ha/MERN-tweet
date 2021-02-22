c898a738b2a0fbe4d8a4277ce615b6dc84ffb003
Michelle Ha <michelle1992us@yahoo.com>
2021-02-21 18:34:37 -0800

    finish frontend tweets

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin:EBZ7bsNNz3XMBLIU@mern.egdfr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secretOrKey: "secret" //use randomkeygen for better secret key
+}
